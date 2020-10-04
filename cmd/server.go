1413a043a7a1888d3517cc2bbd5a9b8faca692b8
timkra <44801440+timkra@users.noreply.github.com>
2020-10-04 10:30:00 +0200

    Update Pingdom API to 3.1

--- cmd/server.go
+++ cmd/server.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	serverCmd = &cobra.Command{
-		Use:   "server [username] [password] [api-key]",
+		Use:   "server [api-token]",
 		Short: "Start the HTTP server",
 		Run:   serverRun,
 	}
@@ -63,19 +63,10 @@ func serverRun(cmd *cobra.Command, args []string) {
 	var client *pingdom.Client
 	flag.Parse()
 
-	if len(cmd.Flags().Args()) == 3 {
-		client = pingdom.NewClient(
-			flag.Arg(1),
-			flag.Arg(2),
-			flag.Arg(3),
-		)
-	} else if len(cmd.Flags().Args()) == 4 {
-		client = pingdom.NewMultiUserClient(
-			flag.Arg(1),
-			flag.Arg(2),
-			flag.Arg(3),
-			flag.Arg(4),
-		)
+	if len(cmd.Flags().Args()) == 1 {
+		client, _ = pingdom.NewClientWithConfig(pingdom.ClientConfig{
+			APIToken: flag.Arg(1),
+		})
 	} else {
 		cmd.Help()
 		os.Exit(1)
